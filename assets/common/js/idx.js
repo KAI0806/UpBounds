2d36bc48ac30fda4a38108ed6a91e7c3b4046023
MASATAKE KAI <nerotan0806@gmail.com>
2021-05-12 15:37:05 +0900

    ABOUTの画像が表示されるようにJSを編集

--- assets/common/js/idx.js
+++ assets/common/js/idx.js
@@ -55,12 +55,12 @@ $(function(){
 });
 
 
-$(function(){
-	var retinaCheck = window.devicePixelRatio;
-	if(retinaCheck >= 2) { // Retinaディスプレイのときを分岐させている
-		$('img.retina').each( function() {
-			var retinaimg = $(this).attr('src').replace(/\.(?=(?:png|jpg|jpeg)$)/i, '@2x.');
-			$(this).attr('srcset', retinaimg + " 2x");
-		});
-	}
-});
+// $(function(){
+// 	var retinaCheck = window.devicePixelRatio;
+// 	if(retinaCheck >= 2) { // Retinaディスプレイのときを分岐させている
+// 		$('img.retina').each( function() {
+// 			var retinaimg = $(this).attr('src').replace(/\.(?=(?:png|jpg|jpeg)$)/i, '@2x.');
+// 			$(this).attr('srcset', retinaimg + " 2x");
+// 		});
+// 	}
+// });
